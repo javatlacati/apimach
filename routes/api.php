9d56c870458ee99ef36a36ff6a1efabfba7a906f
Javatlacati <scherzo_16@hotmail.com>
2024-05-28 17:55:56 -0600

    routes configuration

---
+++ routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CellDetailsController;
+use App\Http\Controllers\CellLocationController;
+use App\Http\Controllers\GroupedCityController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::resource('users', UserController::class);
+
+Route::resource('authors', AuthorController::class);
+
+Route::resource('books', BookController::class);
+
+Route::get('/booksbyauthor', [BookController::class,'showBookByAuthorName']);
+
+Route::resource('cell_details', CellDetailsController::class);
+
+Route::resource('cell_locations', CellLocationController::class);
+
+Route::resource('grouped_cities', GroupedCityController::class);
+
+Route::resource('items', ItemController::class);
